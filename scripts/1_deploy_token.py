b5be389c02936f875c37c13314e09698b5966d58
utku ünal <unal-utku@hotmail.com>
2022-05-26 13:10:00 -0700

    Add files via upload

---
+++ scripts/1_deploy_token.py
@@ -0,0 +1,17 @@
+from brownie import accounts, config, TokenERC20, EasyToken
+from scripts.helpful_scripts import get_account
+import time
+
+
+initial_supply = 1000000000000000000000000  # 1000
+token_name = "KUTUCOIN"
+token_symbol = "KUTU"
+
+
+def main():
+    account = get_account()
+    erc20 = TokenERC20.deploy(
+        initial_supply, token_name, token_symbol, {"from": account}, publish_source=True
+    )
+
+time.sleep(1)
